d50c0ff54914c9a1aa0e7889d497e20d6426c4fb
Konrad Cegalrski <ceg.kond@gmail.com>
2023-03-12 04:08:17 +0100

    lab_02 initialized

---
+++ lab_02/lab_02.cpp
@@ -0,0 +1,18 @@
+// PSiO Poznan University Of Technology
+// Source: https://jug.dpieczynski.pl/lab-psio/Lab%2002%20-%20Algorytmy.html
+// Executed by Konrad Ceglarski
+// 01/03/2023
+
+#include <iostream>
+
+#include <windows.h> // UTF-8 encoding for Windows
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+
+
+    return 0;
+}
